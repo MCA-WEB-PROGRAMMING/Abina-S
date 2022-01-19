026f6d84973daaaa05971250efb6cf34e0478c10
Abina S <94121200+Abina1234@users.noreply.github.com>
2022-01-19 19:29:26 +0530

    Add files via upload

---
+++ student array.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <?php
+        $a=array("Anu","Manya","Steffi","Gopika","Arun","Soni");
+        echo "Array:";
+        print_r($a);
+        echo "<br>"."Array in ascending order:";
+        asort($a);
+        print_r($a);
+        echo "<br>"."Array in descending order:";
+        arsort($a);
+        print_r($a);
+        ?>
+        </body>
+        </html>
+        
+     
